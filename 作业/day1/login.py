b30f93a893d15828ea4981616baa6c9582432735
327101303 <327101303@qq.com>
2016-06-13 09:31:18 +0800

    list,putle

---
+++ 作业/day1/login.py
@@ -0,0 +1,6 @@
+user  = open("users.db","r")
+Openfile = user.readline()
+
+for i in range(3):
+    user_name = raw_input("Please input your username:")
+
---
+++ 作业/day1/test.py
@@ -0,0 +1,7 @@
+f = open("users.db","r")
+openfile = f.readline()
+print openfile
+# for n in openfile:
+
+
+f.close()
