327fcd0fad2f398322f34644abb6b2488bd80fea
327101303 <327101303@qq.com>
2016-06-13 11:02:45 +0800

    list,putle

--- 作业/day1/login.py
+++ 作业/day1/login.py
@@ -3,4 +3,5 @@ Openfile = user.readline()
 
 for i in range(3):
     user_name = raw_input("Please input your username:")
+    for
 
--- 作业/day1/test.py
+++ 作业/day1/test.py
@@ -1,6 +1,6 @@
-f = open("users.db","r")
-openfile = f.readline()
-print openfile
+f = open("C:\Users\zhangjun\PycharmProjects\s12\day1\users.db","r")
+openfile = f.readlines()
+print(f.read())
 # for n in openfile:
 
 
