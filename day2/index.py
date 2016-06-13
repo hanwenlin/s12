ad98bc087f98cff686444d7d81f5d0e1f8b6e7ab
327101303 <327101303@qq.com>
2016-06-13 17:58:13 +0800

    day2

--- day2/index.py
+++ day2/index.py
@@ -1,10 +1,22 @@
-# import sys
+# # import sys
+# #
+# # print(sys.argv)
 #
-# print(sys.argv)
-
-
-t1 = (1,2, {"k1":"v1"})
+#
+# t1 = (1, 2, {"k1":"v1"})
+# # print(t1)
+# # t1[0] = 3
+# t1[2]["k1"] = "b1"
 # print(t1)
-# t1[0] = 3
-t1[2]["k1"] = "b1"
-print(t1)
+# l1 = int
+# print(1+1)
+# age = 19
+# result = age.__rdivmod__(10)
+# print(result)
+# print(19/10)
+#
+# print(19+10)
+bue = -15
+kep = bue.__abs__()
+print(abs(-100))
+print(kep)
