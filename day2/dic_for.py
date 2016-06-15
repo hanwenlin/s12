ea447a09c8648ca632d074433a697f8b7b47c6b5
327101303 <327101303@qq.com>
2016-06-15 17:52:13 +0800

    list,tuple,dic用法

---
+++ day2/dic_for.py
@@ -0,0 +1,15 @@
+dic = {}           #创建一个空字典
+all_list = [11,22,33,44,55,66,77,88,99,] #创建一个列表
+for i in all_list:                  #for循环去遍历整个列表中的每个元素
+    if i > 60:                       #把大于60的元素取出来
+        if 'k1'in  dic.keys():      #判断key'k1'是否在字典里
+            dic['k1'].append(i)      #如果k1在字典的keys中则把i追加到k1列表中
+        else:
+            dic['k1'] = []        #如果k1不在dic字典的keys中，则在字典dic中创建key=k1,k1的valus为列表，列表中包含了i或者一个空list
+    else:
+        if  'k2' in dic.keys():
+            dic['k2'].append(i)
+            pass
+        else:
+            dic['k2'] = []
+print(dic.items())              #输出dic字典的key对应的valus
--- day2/index.py
+++ day2/index.py
@@ -27,4 +27,6 @@
 # list.append
 
 l1 = [11,22,33,44,55,66,77,99,00.1]
-pop(l1)
+l1.pop()
+tuple
+dict
